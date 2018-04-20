cc12fb9317f9bf1acad4e9195034e66bfd77940a
Felipe Noronha <felipenoris@gmail.com>
2018-04-20 18:44:29 -0300

    moving code around

--- src/read.jl
+++ src/read.jl
@@ -57,3 +57,100 @@ function check_minimum_requirements(xf::XLSXFile)
 
     nothing
 end
+
+"""
+Parses package level relationships defined in `_rels/.rels`.
+Prases workbook level relationships defined in `xl/_rels/workbook.xml.rels`.
+"""
+function parse_relationships!(xf::XLSXFile)
+    xroot = xmlroot(xf, "_rels/.rels")
+    @assert LightXML.name(xroot) == "Relationships" "Malformed XLSX file $(xf.filepath). _rels/.rels root node name should be `Relationships`. Found $(LightXML.name(xroot))."
+    #@assert LightXML.attribute(xroot, "xmlns") == "http://schemas.openxmlformats.org/package/2006/relationships" "Unsupported schema for Relationships: $(LightXML.attribute(xroot, "xmlns"))."
+
+    for el in xroot["Relationship"]
+        push!(xf.relationships, Relationship(el))
+    end
+
+    xroot = xmlroot(xf, "xl/_rels/workbook.xml.rels")
+    @assert LightXML.name(xroot) == "Relationships" "Malformed XLSX file $(xf.filepath). xl/_rels/workbook.xml.rels root node name should be `Relationships`. Found $(LightXML.name(xroot))."
+    #@assert LightXML.attribute(xroot, "xmlns") == "http://schemas.openxmlformats.org/package/2006/relationships" "Unsupported schema for Relationships: $(LightXML.attribute(xroot, "xmlns"))."
+
+    for el in xroot["Relationship"]
+        push!(xf.workbook.relationships, Relationship(el))
+    end
+
+    nothing
+end
+
+"""
+  parse_workbook!(xf::XLSXFile)
+
+Updates xf.workbook from xf.data[\"xl/workbook.xml\"]
+"""
+function parse_workbook!(xf::XLSXFile)
+    xroot = xmlroot(xf, "xl/workbook.xml")
+    @assert LightXML.name(xroot) == "workbook" "Malformed xl/workbook.xml. Root node name should be 'workbook'. Got '$(LightXML.name(xroot))'."
+
+    # workbook to be parsed
+    workbook = xf.workbook
+
+    # workbookPr
+    vec_workbookPr = xroot["workbookPr"]
+    if length(vec_workbookPr) > 0
+        @assert length(vec_workbookPr) == 1 "Malformed workbook. $xf has more than 1 workbookPr nodes in xl/workbook.xml."
+
+        workbookPr_element = vec_workbookPr[1]
+        if LightXML.has_attribute(workbookPr_element, "date1904")
+            attribute_value_date1904 = LightXML.attribute(workbookPr_element, "date1904")
+
+            if attribute_value_date1904 == "1" || attribute_value_date1904 == "true"
+                workbook.date1904 = true
+            elseif attribute_value_date1904 == "0" || attribute_value_date1904 == "false"
+                workbook.date1904 = false
+            else
+                error("Could not parse xl/workbook -> workbookPr -> date1904 = $(attribute_value_date1904).")
+            end
+        else
+            # does not have attribute => is not date1904
+            workbook.date1904 = false
+        end
+    end
+
+    # shared string table
+    SHARED_STRINGS_RELATIONSHIP_TYPE = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/sharedStrings"
+    if has_relationship_by_type(workbook, SHARED_STRINGS_RELATIONSHIP_TYPE)
+        sst_root = xmlroot(xf, "xl/" * get_relationship_target_by_type(workbook, SHARED_STRINGS_RELATIONSHIP_TYPE))
+        @assert LightXML.name(sst_root) == "sst" "Malformed workbook. sst file should have sst root."
+        workbook.sst = sst_root["si"]
+    end
+
+    # sheets
+    vec_sheets = xroot["sheets"]
+    if length(vec_sheets) > 0
+        @assert length(vec_sheets) == 1 "Malformed workbook. $xf has more than 1 sheet node in xl/workbook.xml."
+
+        sheets_element = vec_sheets[1]
+
+        vec_sheet = sheets_element["sheet"]
+        num_sheets = length(vec_sheet)
+        workbook.sheets = Vector{Worksheet}(num_sheets)
+
+        for (index, sheet_element) in enumerate(vec_sheet)
+            worksheet = Worksheet(xf, sheet_element)
+            workbook.sheets[index] = worksheet
+        end
+    end
+
+    # styles
+    STYLES_RELATIONSHIP_TYPE = "http://schemas.openxmlformats.org/officeDocument/2006/relationships/styles"
+    if has_relationship_by_type(workbook, STYLES_RELATIONSHIP_TYPE)
+        styles_target = get_relationship_target_by_type(workbook, STYLES_RELATIONSHIP_TYPE)
+        workbook.styles = xmldocument(xf, "xl/" * styles_target)
+
+        # check root node name for styles.xml
+        styles_root = LightXML.root(workbook.styles)
+        @assert LightXML.name(styles_root) == "styleSheet" "Malformed package. Expected root node named `styleSheet` in `styles.xml`."
+    end
+
+    nothing
+end
--- src/relationship.jl
+++ src/relationship.jl
@@ -9,30 +9,6 @@ function Relationship(e::LightXML.XMLElement) :: Relationship
     )
 end
 
-"""
-Parses package level relationships defined in `_rels/.rels`.
-Prases workbook level relationships defined in `xl/_rels/workbook.xml.rels`.
-"""
-function parse_relationships!(xf::XLSXFile)
-    xroot = xmlroot(xf, "_rels/.rels")
-    @assert LightXML.name(xroot) == "Relationships" "Malformed XLSX file $(xf.filepath). _rels/.rels root node name should be `Relationships`. Found $(LightXML.name(xroot))."
-    #@assert LightXML.attribute(xroot, "xmlns") == "http://schemas.openxmlformats.org/package/2006/relationships" "Unsupported schema for Relationships: $(LightXML.attribute(xroot, "xmlns"))."
-
-    for el in xroot["Relationship"]
-        push!(xf.relationships, Relationship(el))
-    end
-
-    xroot = xmlroot(xf, "xl/_rels/workbook.xml.rels")
-    @assert LightXML.name(xroot) == "Relationships" "Malformed XLSX file $(xf.filepath). xl/_rels/workbook.xml.rels root node name should be `Relationships`. Found $(LightXML.name(xroot))."
-    #@assert LightXML.attribute(xroot, "xmlns") == "http://schemas.openxmlformats.org/package/2006/relationships" "Unsupported schema for Relationships: $(LightXML.attribute(xroot, "xmlns"))."
-
-    for el in xroot["Relationship"]
-        push!(xf.workbook.relationships, Relationship(el))
-    end
-
-    nothing
-end
-
 function get_relationship_target_by_id(wb::Workbook, Id::String) :: String
     for r in wb.relationships
         if Id == r.Id
