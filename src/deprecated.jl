daf9ca6ee31e94bef877377b85fe945f2c2e2427
Felipe Noronha <felipenoris@gmail.com>
2018-05-11 10:29:00 -0300

    add Hint on deprecated XLSX.read method.

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -1,5 +1,5 @@
 
 function read(s::AbstractString)
-	warn("`XLSX.read(filepath)` is deprecated. Use `XLSX.readxlsx(filepath)`.")
+	warn("`XLSX.read(filepath)` is deprecated. Use `XLSX.readxlsx(filepath)` instead. Hint: also consider using `XLSX.openxlsx(filepath)` for loading contents on demand.")
 	readxlsx(s)
 end
