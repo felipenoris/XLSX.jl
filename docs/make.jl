a065eab4efd4601cb5f85d18f77f4d7fbd435e6d
Felipe Noronha <felipenoris@gmail.com>
2018-09-03 14:37:36 -0300

    update deploydocs to Julia v1.0

--- docs/make.jl
+++ docs/make.jl
@@ -4,16 +4,14 @@ using Documenter, XLSX
 makedocs(
     format = :html,
     sitename = "XLSX.jl",
-    modules = [XLSX],
-    pages = ["index.md",
-            "api.md"
-             ]
+    modules = [ XLSX ],
+    pages = [ "index.md", "api.md" ]
 )
 
 deploydocs(
     repo = "github.com/felipenoris/XLSX.jl.git",
     target = "build",
-    julia  = "0.6",
+    julia  = "1.0",
     deps   = nothing,
     make   = nothing
 )
