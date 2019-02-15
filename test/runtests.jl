bd1d55b9893b676b29c2f2dbc97ec26e12d45aaf
김용희 <yongheekim@devsisters.com>
2019-02-15 15:24:27 +0900

    add test for #71 #72

--- test/runtests.jl
+++ test/runtests.jl
@@ -1487,3 +1487,11 @@ end
     @test xf["Test1"]["A1"] == "One"
     @test xf["Test1"]["A2"] == 1
 end
+
+@testset "windows compatibility (#71) (#72)" begin
+    xf = XLSX.open_xlsx_template(joinpath(data_directory, "issue71_72.xlsx"))
+    @test xf["Sheet1"]["A1"] == "One"
+    @test xf["Sheet1"]["A2"] == 1
+
+    @test collect(keys(xf.binary_data)) == ["xl/printerSettings/printerSettings1.bin"]
+end
