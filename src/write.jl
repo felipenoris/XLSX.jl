6556d70347813ae8fb9614d77bb18fe04110f81c
Kenton Van Peursem <kenton.vanpeursem@moremath.ai>
2022-07-23 08:50:55 -0700

    Convertion of Real to Float64

--- src/write.jl
+++ src/write.jl
@@ -294,8 +294,9 @@ function setdata!(ws::Worksheet, ref::CellRef, val::CellValue)
     setdata!(ws, cell)
 end
 
-# Convert AbstractString to concrete String
+# convert AbstractTypes to concrete
 setdata!(ws::Worksheet, ref::CellRef, val::AbstractString) = setdata!(ws, ref, CellValue(ws, convert(String, val)))
+setdata!(ws::Worksheet, ref::CellRef, val::Real) = setdata!(ws, ref, CellValue(ws, convert(Float64, val)))
 
 setdata!(ws::Worksheet, row::Integer, col::Integer, val::CellValue) = setdata!(ws, CellRef(row, col), val)
 
