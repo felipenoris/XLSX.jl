69fe5cbb63712be9b1b8f24d2460c9069e89e78e
Felipe Noronha <felipenoris@gmail.com>
2024-08-31 23:05:08 -0300

    bump Documenter v1

--- docs/make.jl
+++ docs/make.jl
@@ -4,11 +4,13 @@ using Documenter, XLSX
 makedocs(
     sitename = "XLSX.jl",
     modules = [ XLSX ],
-    pages = [ "Home" => "index.md",
-              "Tutorial" => "tutorial.md",
-              "API Reference" => "api.md",
-              "Migration Guides" => "migration.md",
-            ]
+    pages = [
+        "Home" => "index.md",
+        "Tutorial" => "tutorial.md",
+        "API Reference" => "api.md",
+        "Migration Guides" => "migration.md",
+    ],
+    checkdocs=:none,
 )
 
 deploydocs(
