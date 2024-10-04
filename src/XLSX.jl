7343cabde7ac953aa3446ed47336ecd540467ec5
TimG1964 <157401228+TimG1964@users.noreply.github.com>
2024-10-04 12:30:16 +0100

    Remove last trace of ZipFile

--- src/XLSX.jl
+++ src/XLSX.jl
@@ -9,12 +9,7 @@ import EzXML
 import Tables
 import Base.convert
 
-# https://github.com/fhs/ZipFile.jl/issues/39
-if !hasmethod(Base.bytesavailable, Tuple{ZipFile.ReadableFile})
-    Base.bytesavailable(f::ZipFile.ReadableFile) = f.uncompressedsize - f._pos
-end
-
-const SPREADSHEET_NAMESPACE_XPATH_ARG = [ "xpath" => "http://schemas.openxmlformats.org/spreadsheetml/2006/main" ]
+const SPREADSHEET_NAMESPACE_XPATH_ARG = ["xpath" => "http://schemas.openxmlformats.org/spreadsheetml/2006/main"]
 const EXCEL_MAX_COLS = 16_384     # total columns supported by Excel per sheet
 const EXCEL_MAX_ROWS = 1_048_576  # total rows supported by Excel per sheet (including headers)
 
