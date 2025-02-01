2d894278a6f383993bbbf033d26df690476eb911
Tim Gebbels <tim@harpleygebbels.net>
2025-02-01 08:44:46 +0000

    Simplify regex that undoes pretty printing

--- src/write.jl
+++ src/write.jl
@@ -66,7 +66,7 @@ function writexlsx(output_source::Union{AbstractString, IO}, xf::XLSXFile; overw
                 continue
             end
             ZipArchives.zip_newfile(xlsx, f; compress=true)
-            write(xlsx, replace(XML.write(xf.data[f]), r"\n *| *\n" => "")) # Don't want pretty printing.
+            write(xlsx, replace(XML.write(xf.data[f]), r" *\n *" => "")) # Don't want pretty printing.
         end
 
         # write binary files
